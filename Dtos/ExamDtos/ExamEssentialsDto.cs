53065023a32599859ea42d2102d2b263214e7fb0
ap-Camel <ayman.abdwlla@gmail.com>
2022-07-31 23:36:19 +0200

    re aranged the order of the class members to be more in sync with the frontEnd

--- Dtos/ExamDtos/ExamEssentialsDto.cs
+++ Dtos/ExamDtos/ExamEssentialsDto.cs
@@ -2,14 +2,14 @@ namespace exmainationApi.Dtos.ExamDtos {
     public record ExamEssentialsDto {
         public int ID {get; init;}
         public string title {get; init;} = string.Empty;
-        public int timesUsed {get; init;}
-        public int passRate {get; init;}
-        public int numOfQuestions {get; init;}
-        public int duration {get; init;}
-        public int passingValue {get; init;}
-        public int numOfPoints {get; init;}
         public bool active {get; init;}
+        public int passingValue {get; init;}   
+        public int numOfQuestions {get; init;}     
+        public int duration {get; init;}
         public DateTime dateToOpen {get; init;}
+        public int numOfPoints {get; init;}
+        public int passRate {get; init;}
+        public int timesUsed {get; init;}
         public DateTime dateCreated {get; init;}
         public DateTime dateUpdated {get; init;}
     }
